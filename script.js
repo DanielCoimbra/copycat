f083148b3c3aa9e792bc0d1c7556bbb06c666538
sacode <coimbrasantos.d@gmail.com>
2021-01-07 22:24:40 -0300

    Update

--- script.js
+++ script.js
@@ -3,8 +3,11 @@ var nav = document.getElementById("nav");
 window.onscroll = function(){
     if (window.pageYOffset > 100){
         nav.style.background="#F1F1EF";
+        nav.style.boxShadow="0 4px 2px -2px gray";
     }
     else{
         nav.style.background="transparent";
+        nav.style.boxShadow="";
+
     } 
 }
